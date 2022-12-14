81c59dd3ddd379a79924fabac34f7c010cd44377
shokhrukh <shohruxkarimov546@gmail.com>
2022-12-14 04:55:35 -0600

    refactor: adding burger menu

--- pages/donate/menu.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $('#forMenu').click(function () {
-        $('#menu').toggleClass('showmenu');
-    })
-})
--- pages/main/menu.js
+++ pages/main/menu.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-    $('#forMenu').click(function () {
-        $('#menu').toggleClass('showmenu');
-    })
-})
+const menu_icon = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
 
+menu_icon.addEventListener('click', () => {
+    menu_icon.classList.toggle('active');
+    menu.classList.toggle('active');
+    console.log('clicked')
+})
